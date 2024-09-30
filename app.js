e278f81c0c2cc6921914b1ad73d864b2b8e9823b
Adunbi Fortunate <samuelfortunate098@gmail.com>
2024-09-30 14:00:35 +0100

    updated json parse

--- app.js
+++ app.js
@@ -7,19 +7,8 @@ const requestLogger = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-
-// const requestLogger = (request, response, next) => {
-//   console.log(`Method: ${request.method}`)
-//   console.log(`Path: ${request.path}`)
-//   console.log(`Headers: ${JSON.stringify(request.headers)}`)
-//   console.log(`Body: ${JSON.stringify(request.body)}`)
-//   console.log(`---`)
-//   next()
-// }
-
 mongoose.set('strictQuery', false)
 
-// const mongoUrl = 'mongodb+srv://noteUser:notebook@cluster1.hywww.mongodb.net/Blog?retryWrites=true&w=majority'
 mongoose.connect(config.MONGODB_URI)
   .then(()=> {
     logger.info(`connected to ${config.MONGODB_URI}`)
