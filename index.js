0a34ec5add9085c7099aef08a755bf82d5c9f1be
Adunbi Fortunate <samuelfortunate098@gmail.com>
2024-09-30 12:43:17 +0100

    configured the app and index modules

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-
-const requestLogger = (request, response, next) => {
-  console.log(`Method: ${request.method}`)
-  console.log(`Path: ${request.path}`)
-  console.log(`Headers: ${JSON.stringify(request.headers)}`)
-  console.log(`Body: ${JSON.stringify(request.body)}`)
-  console.log(`---`)
-  next()
-}
-
-mongoose.set('strictQuery', false)
-
-const mongoUrl = 'mongodb+srv://noteUser:notebook@cluster1.hywww.mongodb.net/Blog?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-  .then(()=> {
-    console.log(`connected to ${mongoUrl}`)
-  })
-  .catch(error =>{
-    console.error(`Error connecting to mongodb ${error.message}`)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-
-app.use('/api/blogs', blogRouter)
-
+const app = require('./app')
 
 const PORT = 3003
 app.listen(PORT, () => {
